09b3f4876df821a43f1c63365726d632bd9bffc2
lwgray <lwgray@gmail.com>
2016-05-01 21:07:21 -0400

    Setup table

--- add_refunds.py
+++ add_refunds.py
@@ -5,15 +5,16 @@ with sql.connect("tmp/explorer.db") as con:
     data = find_non_returned()
     cur = con.cursor()
     for index, value in data:
-        id = value[1]
         date = value[0]
+        id = value[1]
         sku = value[2]
         trans = value[3]
-        detail = value[4]
-        amount = value[5]
-        quantity = value[6]
-        title = value[7]
-        cur.execute("INSERT INTO refunds (id, date, sku, trans, detail, amount, quantity, title) VALUES (?,?,?,?,?,?,?,?)", (id, date, sku, trans, detail, amount, quantity, title))
+        paytype = value[4]
+        detail = value[5]
+        amount = value[6]
+        quantity = value[7]
+        title = value[8]
+        cur.execute("INSERT INTO refunds (id, date, sku, trans, paytype, detail, amount, quantity, title) VALUES (?,?,?,?,?,?,?,?,?)", (id, date, sku, trans, paytype, detail, amount, quantity, title))
     con.commit()
     msg = "Records successfully added"
     print msg
--- create_db.py
+++ create_db.py
@@ -4,6 +4,7 @@ conn = sqlite3.connect('tmp/explorer.db')
 print "Opened database successfully";
 
 conn.execute('Drop TABLE if exists refunds')
-conn.execute('CREATE TABLE refunds (id TEXT, date TEXT, sku TEXT, trans TEXT, detail TEXT, amount INTEGER, quantity INTEGER, title TEXT)')
+print "Droppred Table - Refunds"
+conn.execute('CREATE TABLE refunds (id TEXT, date TEXT, sku TEXT, trans TEXT, paytype TEXT, detail TEXT, amount INTEGER, quantity INTEGER, title TEXT)')
 print "Table created successfully";
 conn.close()
